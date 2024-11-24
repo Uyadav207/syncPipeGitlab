3f6daa72c7a7eba1644b99aacb86bb523db162fd
Nithin Valiyaveedu <nithinvaliya@gmail.com>
2024-11-25 00:31:26 +0100

    initial setup

---
+++ src/Qest.tsx
@@ -0,0 +1,13 @@
+const Qest = () => {
+	return (
+		<>
+			<div>Qest</div>
+			<div>Qest</div>
+			<div>Qest</div>
+			<div>Qest</div>
+			<div>Qest</div>
+		</>
+	);
+};
+
+export default Qest;
---
+++ src/Test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Test = () => {
+	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+
+export default Test;
